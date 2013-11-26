b44e5375c8776017b34593adb1cb7a280a0165e0
Runner2k11 <abagrov@gmail.com>
2013-11-26 23:20:23 +0400

    Апдейт htaccess и offline страницы

---
+++ offline.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" href="./theme/sunny/jquery-ui.css" type="text/css" media="print, projection, screen" />
+        <link rel="stylesheet" href="./theme/style.css" type="text/css" media="print, projection, screen" />
+        <style>
+            #main {
+                left: 0px;
+                margin: 0 auto;
+                position: relative;
+                height: 100%;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body style="overflow:hidden;overflow-y:hidden">
+        <div id="main" class="ui-accordion-content ui-widget-content ui-accordion-content-active">
+            <table style="width: 100%; height:100%;" cellpadding="4" cellspacing="0" class="ui-widget-content">
+               <tr style="vertical-align: bottom;">
+                  <td align="center" style="height: 50%; width:500px;">
+
+<div class="ui-accordion-content ui-widget-content ui-accordion-content-active">
+  <table style="border-width: 0; " cellpadding="0" cellspacing="0" width="450px" class="ui-widget-content">
+  <tbody>
+    <tr>
+       <td>&#1042; &#1089;&#1074;&#1103;&#1079;&#1080; &#1089; &#1088;&#1072;&#1073;&#1086;&#1090;&#1072;&#1084;&#1080; &#1087;&#1086; &#1087;&#1077;&#1088;&#1077;&#1093;&#1086;&#1076;&#1091; &#1085;&#1072; API v2.0 &#1089;&#1072;&#1081;&#1090; &#1089;&#1090;&#1072;&#1090;&#1080;&#1089;&#1090;&#1080;&#1082;&#1080; &#1074;&#1088;&#1077;&#1084;&#1077;&#1085;&#1085;&#1086; &#1085;&#1077; &#1088;&#1072;&#1073;&#1086;&#1090;&#1072;&#1077;&#1090;.</td>
+    </tr>
+  </tbody>
+  </table>
+</div>
+
+                  </td>
+               </tr>
+
+ <tr style="vertical-align: top;">
+<td align="center">
+<div class="ui-state-highlight ui-corner-all">
+<img src="offline.jpg">
+</div>
+</td>
+</tr>
+
+            </table>
+        </div>
+    </body>
+</html>
