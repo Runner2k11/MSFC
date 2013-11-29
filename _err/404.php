bcf968cc72df84e4f7f25827223e21acd3c64591
Runner2k11 <abagrov@gmail.com>
2013-11-29 18:33:59 +0400

    Страница 404

---
+++ _err/404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" href="../theme/sunny/jquery-ui.css" type="text/css" media="print, projection, screen" />
+        <link rel="stylesheet" href="../theme/style.css" type="text/css" media="print, projection, screen" />
+        <style>
+            #main {
+                left: 0px;
+                margin: 0 auto;
+                position: relative;
+                height: 100%;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body style="overflow:hidden;overflow-y:hidden">
+        <div id="main" class="ui-accordion-content ui-widget-content ui-accordion-content-active">
+            <table style="width: 100%; height:100%;" cellpadding="4" cellspacing="0" class="ui-widget-content">
+               <tr style="vertical-align: bottom;">
+                  <td align="center" style="height: 50%; width:500px;">
+                    <div style="width:450px;" class="ui-accordion-content ui-widget-content ui-accordion-content-active">
+                      Запрашиваемая страница не найдена
+                    </div>
+                  </td>
+               </tr>
+               <tr style="vertical-align: top;">
+                  <td align="center">
+                     <div style="font-size: 30px; width:450px;" class="ui-state-highlight ui-corner-all col_red">
+                       Ошибка 404
+                     </div>
+                  </td>
+               </tr>
+            </table>
+        </div>
+    </body>
+</html>
