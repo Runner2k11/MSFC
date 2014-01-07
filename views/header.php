2db9a3ce9391b35c4349a19046b5a8442a40baab
Runner2k11 <abagrov@gmail.com>
2014-01-07 15:09:30 +0400

    Фикс после слияния

--- views/header.php
+++ views/header.php
@@ -70,6 +70,7 @@
               .add("#available_tanks").tablesorter();
 
               $("#avt1").tablesorter({headers:{ 0: { sorter: false}, 1: {sorter: false}, 2: {sorter: false}}, widgetOptions: {uitheme : 'bootstrap'}});
+              <? for ($i=2; $i<=6; $i++) { ?>
               $("#avt<?=$i;?>").tablesorter({headers:{ 0: { sorter: false}, 1: {sorter: false} }, sortList:[[1,0]], widgetOptions: {uitheme : 'bootstrap'}});
               <? } ?>
 
