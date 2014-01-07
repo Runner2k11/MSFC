02455e254d25ccfa9378048243b43c707df4d6d5
Runner2k11 <abagrov@gmail.com>
2014-01-07 14:50:39 +0400

    Апдейт мессаги

--- views/messages.php
+++ views/messages.php
@@ -1,3 +1,5 @@
+<?php if (false) { ?>
 <td width="500px" style="padding: 15px"><div class="ui-state-highlight ui-corner-all" style="overflow-y:auto; height:120px; padding:5px;">
   <b>26/11/2013</b> - Апдейт до версии модуля 3.0.0
 </div></td>
+<?php }; ?>
