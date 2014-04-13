cf955bb9ac7b3dd7cbe34df1fa7dcb233d01b666
Runner2k11 <abagrov@gmail.com>
2014-04-13 15:54:40 +0400

    Улучшение фильтра дизайна "Дерева техники"

--- ajax/drevopre.php
+++ ajax/drevopre.php
@@ -1,38 +1,43 @@
 <?php
     /*
-    * Project:     Clan Stat
-    * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
-    * Link:        http://creativecommons.org/licenses/by-nc-sa/3.0/
-    * -----------------------------------------------------------------------
-    * Began:       2012
-    * Date:        $Date: 2012-12-01
-    * -----------------------------------------------------------------------
-    * @author      $Author: SHW $
-    * @copyright   2012-2012 SHW
-    * @link        http://wot-news.com
-    * @package     Clan Stat
-    * @version     $Rev: 2.2.0 $
-    *
+    * @author      abagrov
+    * @copyright   2014 abagrov
     */
 ?>
 
 <?php
+
+    error_reporting(E_ALL & ~E_STRICT);
+    ini_set("display_errors", 1);
+
     $content = file_get_contents('http://armor.kiev.ua/wot/gamertrees/'.$_GET['b_player']);
-    $search = array('/<h1>wot[^.]*<\/h1>/i',
-                    '/<div id="h_counters">.*<\/div>/s',
-                    '/margin: 30px 8px 30px 16px;/',
-                    '/height: inherit;/',
-                    '/body {background-color: #060606;}/',
-                    '/#060606/',
-                    '/#BEBEBE/',
-                    '/255,255,255/');
-    $replace = array('',
-                    '',
-                    'margin: 30px 8px 0px 16px;',
-                    'height: 580px;',
-                    'body {background-color: transparent;}',
-                    'transparent',
-                    'rgb(56, 56, 56)',
-                    '0,0,0');
+    $search = array(
+        '/<h1>wot[^.]*<\/h1>/i',  //отключаем заголовок
+        '/<div id="h_counters">.*<\/div>/s',  //отключаем счётчики
+        '/margin: 30px 8px 30px 16px;/',  //для tree убираем нижний margin
+        '/height: inherit;/',  //фиксируем (уменьшаем без потерь) высоту деревьев техники, чтобы не вылезать за пределы фрейма по вертикали
+        '/#060606/',  //делаем все фоны прозрачными
+        '/#BEBEBE/',  //цвет текста и т.п. = ui-widget-content
+        '/255,255,255/',  //меняем зебру уровней с белой на чёрную
+        '/target="_top"/',  //открываем ссылки на бронесайт в новой вкладке
+        '/(<style.*<\/style>)/s',  //добавляем ссылку на свой стиль JQ
+        '/(attr\(\'class\'\, \'|class=")click/',  //меняем стиль класса li.click
+        '/(attr\(\'class\'\, \'|class=")active/',  //меняем стиль класса li.active
+        '/(#nations li.)active/'  //old = $("#nations li.ui-state-active")
+    );
+    $replace = array(
+        '',
+        '',
+        'margin: 30px 8px 0px 16px;',
+        'height: 580px;',
+        'transparent',
+        'rgb(56, 56, 56)',
+        '0,0,0',
+        'target="_blank"',
+        '$1<link media="print, projection, screen" type="text/css" href="http://'.$_SERVER['HTTP_HOST'].'/theme/sunny/jquery-ui.css" rel="stylesheet"></link>',
+        '$1click ui-corner-top ui-state-default',
+        '$1ui-corner-top ui-state-active',
+        '$1ui-state-active'
+    );
     echo preg_replace($search, $replace, $content);
 ?>
